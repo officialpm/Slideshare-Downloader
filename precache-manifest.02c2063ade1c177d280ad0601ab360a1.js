9adec31498fd4c372c3d7e1f4a08fa594047e9ab
Parth Maniar <officialparthmaniar@gmail.com>
2020-09-17 18:00:21 +0530

    Updates

---
+++ precache-manifest.02c2063ade1c177d280ad0601ab360a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20a621d2f934796c287cab63561d357d",
+    "url": "/Slideshare-Downloader/index.html"
+  },
+  {
+    "revision": "d0ca330eaf1204973393",
+    "url": "/Slideshare-Downloader/static/css/2.6ad13c07.chunk.css"
+  },
+  {
+    "revision": "73dab118c81a6cc1cf1e",
+    "url": "/Slideshare-Downloader/static/css/main.19f36cf7.chunk.css"
+  },
+  {
+    "revision": "d0ca330eaf1204973393",
+    "url": "/Slideshare-Downloader/static/js/2.9d22e1d1.chunk.js"
+  },
+  {
+    "revision": "6bd732d9fd36e6d716fd9f6d620a8460",
+    "url": "/Slideshare-Downloader/static/js/2.9d22e1d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73dab118c81a6cc1cf1e",
+    "url": "/Slideshare-Downloader/static/js/main.b2b08586.chunk.js"
+  },
+  {
+    "revision": "9c0804bee0562f1c446d",
+    "url": "/Slideshare-Downloader/static/js/runtime-main.2974585d.js"
+  },
+  {
+    "revision": "2b98972b6e250dcc5570c19d94bcaf23",
+    "url": "/Slideshare-Downloader/static/media/download.2b98972b.svg"
+  }
+]);
